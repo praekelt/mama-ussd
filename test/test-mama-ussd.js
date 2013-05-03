f6cd99f58950aa61badad35cd404140d471d0c31
Mike Jones <mike@westerncapelabs.com>
2013-05-03 16:01:38 +0200

    Pushing in broken state for review. Added dynamic state creation.

--- test/test-mama-ussd.js
+++ test/test-mama-ussd.js
@@ -291,252 +291,252 @@ describe("test_mama_ussd", function() {
             );
     });
 
-    it("unregistered users - unknown should exit with message (part 1)", function () {
-        tester.check_state(null, "3", "register_all_endstate",
-            "^If you have missed a period and have 1 or more of these, do a pregnancy test: "+
-            "nausea or vomiting; tender breasts; often tired.[^]"+
-            "1. Read more$"
-            );
-    });
+    // it("unregistered users - unknown should exit with message (part 1)", function () {
+    //     tester.check_state(null, "3", "register_all_endstate",
+    //         "^If you have missed a period and have 1 or more of these, do a pregnancy test: "+
+    //         "nausea or vomiting; tender breasts; often tired.[^]"+
+    //         "1. Read more$"
+    //         );
+    // });
 
-    it("unregistered users - unknown should exit with message (part 2)", function () {
-        var user = {
-            current_state: 'register_all_endstate',
-            answers: {
-                register_all_1: 'dontknow'
-            }
-        };
-        tester.check_state(user,
-            '1',
-            "register_all_endstate2",
-            "^Don't wait! The first pregnancy check-up must happen as soon as you " +
-            "know. Do the test as soon as possible at any clinic, or get one at a " +
-            "pharmacy. Stay well.$");
-    });
+    // it("unregistered users - unknown should exit with message (part 2)", function () {
+    //     var user = {
+    //         current_state: 'register_all_endstate',
+    //         answers: {
+    //             register_all_1: 'dontknow'
+    //         }
+    //     };
+    //     tester.check_state(user,
+    //         '1',
+    //         "register_all_endstate2",
+    //         "^Don't wait! The first pregnancy check-up must happen as soon as you " +
+    //         "know. Do the test as soon as possible at any clinic, or get one at a " +
+    //         "pharmacy. Stay well.$");
+    // });
 
 
-    it("unregistered users - prebirth - unknown should exit", function () {
-        var user = {
-            current_state: 'register_prebirth_2',
-            answers: {
-                register_all_1: 'pregnant'
-            }
-        };
-        tester.check_state(user,
-            '13',
-            "register_prebirth_2_endstate",
-            "^To sign up, we need to know which month. Please go to the clinic to " +
-            "find out, and dial us again.$");
-    });
+    // it("unregistered users - prebirth - unknown should exit", function () {
+    //     var user = {
+    //         current_state: 'register_prebirth_2',
+    //         answers: {
+    //             register_all_1: 'pregnant'
+    //         }
+    //     };
+    //     tester.check_state(user,
+    //         '13',
+    //         "register_prebirth_2_endstate",
+    //         "^To sign up, we need to know which month. Please go to the clinic to " +
+    //         "find out, and dial us again.$");
+    // });
 
-    it("unregistered users - postbirth - should be prompted for month old", function () {
-        tester.check_state(null, "2", "register_postbirth_2",
-            "^How many months old is your baby\\?[^]" +
-            "1. 1[^]"+
-            "2. 2[^]" +
-            "3. 3[^]" +
-            "4. 4[^]" +
-            "5. 5[^]" +
-            "6. 6[^]" +
-            "7. 7[^]" +
-            "8. 8[^]" +
-            "9. 9[^]" +
-            "10. 10[^]" +
-            "11. More than 10$"
-            );
-    });
+    // it("unregistered users - postbirth - should be prompted for month old", function () {
+    //     tester.check_state(null, "2", "register_postbirth_2",
+    //         "^How many months old is your baby\\?[^]" +
+    //         "1. 1[^]"+
+    //         "2. 2[^]" +
+    //         "3. 3[^]" +
+    //         "4. 4[^]" +
+    //         "5. 5[^]" +
+    //         "6. 6[^]" +
+    //         "7. 7[^]" +
+    //         "8. 8[^]" +
+    //         "9. 9[^]" +
+    //         "10. 10[^]" +
+    //         "11. More than 10$"
+    //         );
+    // });
 
-    it("unregistered users - postbirth - over 10 months should exit with message", function () {
-        var user = {
-            current_state: 'register_postbirth_2',
-            answers: {
-                register_all_1: 'baby'
-            }
-        };
-        tester.check_state(user,
-            '11',
-            "register_postbirth_2_endstate",
-            "^Sorry, the MAMA quizzes are aimed at mothers of younger babies. " +
-            "You can visit askmama.mobi to read useful info, and meet other moms. Stay well.$");
-    });
+    // it("unregistered users - postbirth - over 10 months should exit with message", function () {
+    //     var user = {
+    //         current_state: 'register_postbirth_2',
+    //         answers: {
+    //             register_all_1: 'baby'
+    //         }
+    //     };
+    //     tester.check_state(user,
+    //         '11',
+    //         "register_postbirth_2_endstate",
+    //         "^Sorry, the MAMA quizzes are aimed at mothers of younger babies. " +
+    //         "You can visit askmama.mobi to read useful info, and meet other moms. Stay well.$");
+    // });
 
-    it("unregistered users - prebirth - get HIV information", function () {
-        var user = {
-            current_state: 'register_prebirth_2',
-            answers: {
-                register_all_1: 'pregnant'
-            }
-        };
-        tester.check_state(user, "1", "register_all_hivinfo",
-            "^Your quiz can include info on HIV. Would you like that\\?[^]" +
-            "1. Yes[^]"+
-            "2. No$"
-            );
-    });
+    // it("unregistered users - prebirth - get HIV information", function () {
+    //     var user = {
+    //         current_state: 'register_prebirth_2',
+    //         answers: {
+    //             register_all_1: 'pregnant'
+    //         }
+    //     };
+    //     tester.check_state(user, "1", "register_all_hivinfo",
+    //         "^Your quiz can include info on HIV. Would you like that\\?[^]" +
+    //         "1. Yes[^]"+
+    //         "2. No$"
+    //         );
+    // });
 
-    it("unregistered users - postbirth - get HIV information", function () {
-        var user = {
-            current_state: 'register_postbirth_2',
-            answers: {
-                register_all_1: 'baby'
-            }
-        };
-        tester.check_state(user, "1", "register_all_hivinfo",
-            "^Your quiz can include info on HIV. Would you like that\\?[^]" +
-            "1. Yes[^]"+
-            "2. No$"
-            );
-    });
+    // it("unregistered users - postbirth - get HIV information", function () {
+    //     var user = {
+    //         current_state: 'register_postbirth_2',
+    //         answers: {
+    //             register_all_1: 'baby'
+    //         }
+    //     };
+    //     tester.check_state(user, "1", "register_all_hivinfo",
+    //         "^Your quiz can include info on HIV. Would you like that\\?[^]" +
+    //         "1. Yes[^]"+
+    //         "2. No$"
+    //         );
+    // });
 
-    it("unregistered users - prebirth - sms opt in", function () {
-        var user = {
-            current_state: 'register_all_hivinfo',
-            answers: {
-                register_all_1: 'pregnant',
-                register_prebirth_2: '10'
-            }
-        };
-        tester.check_state(user, "1", "register_all_smsoptin",
-            "^We can send you sms's to remind you to take the next quiz. Would " +
-            "you like that\\?[^]" +
-            "1. Yes[^]"+
-            "2. No$"
-            );
-    });
+    // it("unregistered users - prebirth - sms opt in", function () {
+    //     var user = {
+    //         current_state: 'register_all_hivinfo',
+    //         answers: {
+    //             register_all_1: 'pregnant',
+    //             register_prebirth_2: '10'
+    //         }
+    //     };
+    //     tester.check_state(user, "1", "register_all_smsoptin",
+    //         "^We can send you sms's to remind you to take the next quiz. Would " +
+    //         "you like that\\?[^]" +
+    //         "1. Yes[^]"+
+    //         "2. No$"
+    //         );
+    // });
 
-    it("unregistered users - postbirth - sms opt in", function () {
-        var user = {
-            current_state: 'register_all_hivinfo',
-            answers: {
-                register_all_1: 'baby',
-                register_postbirth_2: '1'
-            }
-        };
-        tester.check_state(user, "1", "register_all_smsoptin",
-            "^We can send you sms's to remind you to take the next quiz. Would " +
-            "you like that\\?[^]" +
-            "1. Yes[^]"+
-            "2. No$"
-            );
-    });
+    // it("unregistered users - postbirth - sms opt in", function () {
+    //     var user = {
+    //         current_state: 'register_all_hivinfo',
+    //         answers: {
+    //             register_all_1: 'baby',
+    //             register_postbirth_2: '1'
+    //         }
+    //     };
+    //     tester.check_state(user, "1", "register_all_smsoptin",
+    //         "^We can send you sms's to remind you to take the next quiz. Would " +
+    //         "you like that\\?[^]" +
+    //         "1. Yes[^]"+
+    //         "2. No$"
+    //         );
+    // });
 
-    it("unregistered users - prebirth - thanks and want quiz", function () {
-        var user = {
-            current_state: 'register_all_smsoptin',
-            answers: {
-                register_all_1: 'pregnant',
-                register_prebirth_2: '1',
-                register_all_hivinfo: 'yes'
-            }
-        };
-        tester.check_state(user, "1", "register_all_thanksandstart",
-            "^Thank you! You can now start to learn by taking your first quiz. " +
-            "Start now\\?[^]" +
-            "1. Yes[^]"+
-            "2. No$"
-            );
-    });
+    // it("unregistered users - prebirth - thanks and want quiz", function () {
+    //     var user = {
+    //         current_state: 'register_all_smsoptin',
+    //         answers: {
+    //             register_all_1: 'pregnant',
+    //             register_prebirth_2: '1',
+    //             register_all_hivinfo: 'yes'
+    //         }
+    //     };
+    //     tester.check_state(user, "1", "register_all_thanksandstart",
+    //         "^Thank you! You can now start to learn by taking your first quiz. " +
+    //         "Start now\\?[^]" +
+    //         "1. Yes[^]"+
+    //         "2. No$"
+    //         );
+    // });
 
-    it("unregistered users - postbirth - thanks and want quiz", function () {
-        var user = {
-            current_state: 'register_all_smsoptin',
-            answers: {
-                register_all_1: 'baby',
-                register_prebirth_2: '1',
-                register_all_hivinfo: 'yes'
-            }
-        };
-        tester.check_state(user, "1", "register_all_thanksandstart",
-            "^Thank you! You can now start to learn by taking your first quiz. " +
-            "Start now\\?[^]" +
-            "1. Yes[^]"+
-            "2. No$"
-            );
-    });
+    // it("unregistered users - postbirth - thanks and want quiz", function () {
+    //     var user = {
+    //         current_state: 'register_all_smsoptin',
+    //         answers: {
+    //             register_all_1: 'baby',
+    //             register_prebirth_2: '1',
+    //             register_all_hivinfo: 'yes'
+    //         }
+    //     };
+    //     tester.check_state(user, "1", "register_all_thanksandstart",
+    //         "^Thank you! You can now start to learn by taking your first quiz. " +
+    //         "Start now\\?[^]" +
+    //         "1. Yes[^]"+
+    //         "2. No$"
+    //         );
+    // });
 
-    it("unregistered users - prebirth - end success", function () {
-        var user = {
-            current_state: 'register_all_thanksandstart',
-            answers: {
-                register_all_1: 'pregnant',
-                register_prebirth_2: '1',
-                register_all_hivinfo: 'yes',
-                register_all_smsoptin: 'yes'
-            }
-        };
-        tester.check_state(user, "2", "register_all_endsuccess",
-            "^Thanks for joining MAMA. Dial \\*120\\*2112\\# each week to start " +
-            "learning about your growing baby.$"
-            );
-    });
+    // it("unregistered users - prebirth - end success", function () {
+    //     var user = {
+    //         current_state: 'register_all_thanksandstart',
+    //         answers: {
+    //             register_all_1: 'pregnant',
+    //             register_prebirth_2: '1',
+    //             register_all_hivinfo: 'yes',
+    //             register_all_smsoptin: 'yes'
+    //         }
+    //     };
+    //     tester.check_state(user, "2", "register_all_endsuccess",
+    //         "^Thanks for joining MAMA. Dial \\*120\\*2112\\# each week to start " +
+    //         "learning about your growing baby.$"
+    //         );
+    // });
 
-    it("unregistered users - postbirth - end success", function () {
-        var user = {
-            current_state: 'register_all_thanksandstart',
-            answers: {
-                register_all_1: 'baby',
-                register_prebirth_2: '1',
-                register_all_hivinfo: 'yes',
-                register_all_smsoptin: 'yes'
-            }
-        };
-        tester.check_state(user, "2", "register_all_endsuccess",
-            "^Thanks for joining MAMA. Dial \\*120\\*2112\\# each week to start " +
-            "learning about your growing baby.$"
-            );
-    });
+    // it("unregistered users - postbirth - end success", function () {
+    //     var user = {
+    //         current_state: 'register_all_thanksandstart',
+    //         answers: {
+    //             register_all_1: 'baby',
+    //             register_prebirth_2: '1',
+    //             register_all_hivinfo: 'yes',
+    //             register_all_smsoptin: 'yes'
+    //         }
+    //     };
+    //     tester.check_state(user, "2", "register_all_endsuccess",
+    //         "^Thanks for joining MAMA. Dial \\*120\\*2112\\# each week to start " +
+    //         "learning about your growing baby.$"
+    //         );
+    // });
 
-    it("just registered users - prebirth - start quiz", function () {
-        var user = {
-            current_state: 'register_all_thanksandstart',
-            answers: {
-                register_all_1: 'pregnant',
-                register_prebirth_2: '1',
-                register_all_hivinfo: 'yes',
-                register_all_smsoptin: 'yes'
-            }
-        };
-        tester.check_state(user, "1", "quiz_start",
-            "^Congrats on your pregnancy! What kind of foods should you eat now\\?[^]" +
-            "1. Fruit and vegetables[^]"+
-            "2. Chips and soda$"
-            );
-    });
+    // it("just registered users - prebirth - start quiz", function () {
+    //     var user = {
+    //         current_state: 'register_all_thanksandstart',
+    //         answers: {
+    //             register_all_1: 'pregnant',
+    //             register_prebirth_2: '1',
+    //             register_all_hivinfo: 'yes',
+    //             register_all_smsoptin: 'yes'
+    //         }
+    //     };
+    //     tester.check_state(user, "1", "quiz_start",
+    //         "^Congrats on your pregnancy! What kind of foods should you eat now\\?[^]" +
+    //         "1. Fruit and vegetables[^]"+
+    //         "2. Chips and soda$"
+    //         );
+    // });
 
-    it("just registered users - postbirth - start quiz", function () {
-        var user = {
-            current_state: 'register_all_thanksandstart',
-            answers: {
-                register_all_1: 'baby',
-                register_prebirth_2: '1',
-                register_all_hivinfo: 'yes',
-                register_all_smsoptin: 'yes'
-            }
-        };
-        tester.check_state(user, "1", "quiz_start",
-            "^Congrats on your pregnancy! What kind of foods should you eat now\\?[^]" +
-            "1. Fruit and vegetables[^]"+
-            "2. Chips and soda$"
-            );
-    });
+    // it("just registered users - postbirth - start quiz", function () {
+    //     var user = {
+    //         current_state: 'register_all_thanksandstart',
+    //         answers: {
+    //             register_all_1: 'baby',
+    //             register_prebirth_2: '1',
+    //             register_all_hivinfo: 'yes',
+    //             register_all_smsoptin: 'yes'
+    //         }
+    //     };
+    //     tester.check_state(user, "1", "quiz_start",
+    //         "^Congrats on your pregnancy! What kind of foods should you eat now\\?[^]" +
+    //         "1. Fruit and vegetables[^]"+
+    //         "2. Chips and soda$"
+    //         );
+    // });
 
-    it("just registered users - prebirth - answer quiz", function () {
-        var user = {
-            current_state: 'quiz_start',
-            answers: {
-                register_all_1: 'pregnant',
-                register_prebirth_2: '1',
-                register_all_hivinfo: 'yes',
-                register_all_smsoptin: 'yes',
-                quiz_start: 'q_1_a_1'
-            }
-        };
-        tester.check_state(user, "1", "q_1_a_1",
-            "^Yes! It's time to eat plenty of healthy fruits and vegetables to" +
-            " nourish your growing baby.[^]" +
-            "1. Next$"
-            );
-    });
+    // it("just registered users - prebirth - answer quiz", function () {
+    //     var user = {
+    //         current_state: 'quiz_start',
+    //         answers: {
+    //             register_all_1: 'pregnant',
+    //             register_prebirth_2: '1',
+    //             register_all_hivinfo: 'yes',
+    //             register_all_smsoptin: 'yes',
+    //             quiz_start: 'q_1_a_1'
+    //         }
+    //     };
+    //     tester.check_state(user, "1", "q_1_a_1",
+    //         "^Yes! It's time to eat plenty of healthy fruits and vegetables to" +
+    //         " nourish your growing baby.[^]" +
+    //         "1. Next$"
+    //         );
+    // });
 
     // first quiz test should always start 'null, null' because we haven't started interacting yet
     // it("registered users - prebirth - load week 1", function () {
